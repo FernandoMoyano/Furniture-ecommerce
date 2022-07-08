ff97651444c19fcbc342bedba77f3e0753c0163b
FernandoMoyano <fernandomoyano21@gmail.com>
2022-07-08 17:42:22 -0300

    get product from products-add product to the cart-save cart local storage-set cart values

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const productsDOM = document.querySelector(".products-center");
 
 //cart
 let cart = [];
+//buttons
+let buttonsDOM
 
 
 //getting the products
@@ -73,7 +75,7 @@ class UI {
 
     getBagButtons() {
         const buttons = [...document.querySelectorAll(".bag-btn")];
-        console.log(buttons);
+        buttonsDOM = buttons;
 
         buttons.forEach(button => {
             let id = button.dataset.id;
@@ -83,17 +85,36 @@ class UI {
                 button.disabled = true;
             } else {
                 button.addEventListener('click', (event) => {
-                    event.target.innerText="in Cart";
-                    event.target.disabled=true;
+                    event.target.innerText = "in Cart";
+                    event.target.disabled = true;
                     //get product from products
+                    let cartItem = {
+                        ...Storage.getProduct(id),
+                        amount: 1
+                    };
                     //add product to the cart
+                    cart = [...cart, cartItem];
+                    console.log(cart);
                     //save cart in local storage
+                    Storage.saveCart(cart)
                     //set cart values
+                    this.setCartValues(cart);
                     //dispplay cart item
                     //show the cart
                 })
             }
         });
+    }
+    setCartValues(cart) {
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map(item => {
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount
+        })
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2))
+        cartItems.innerText = itemsTotal;
+        console.log(cartTotal, cartItems);
     }
 }
 
@@ -101,6 +122,13 @@ class UI {
 class Storage {
     static saveProducts(products) {
         localStorage.setItem("products", JSON.stringify(products));
+    }
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem('products'));
+        return products.find(product => product.id === id);
+    }
+    static saveCart(cart) {
+        localStorage.setItem('cart', JSON.stringify(cart));
     }
 }
 
